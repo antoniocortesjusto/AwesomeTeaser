70304ed26690206bb0b0fc3577271d98eca1bcf3
Antonio Cortes <antonio@awesome.do>
2013-10-29 01:40:05 +0100

    Share buttons

--- js/controllers.js
+++ js/controllers.js
@@ -17,6 +17,7 @@ controllers.controller('HomeCtrl', function($scope, $route, MailChimp){
 	
   $scope.Subscription ={};
   $scope.Subscription.subscriptionDone = false;
+  $scope.tumblrURL = "";
 
   $scope.User ={email: '', group: ''};
   	var handlePingSuccess = function(data, status){
@@ -33,6 +34,7 @@ controllers.controller('HomeCtrl', function($scope, $route, MailChimp){
       console.log("MailChimp.subscribe() Post success ==> " + "data.groups: " + data + " status: " + status);
         $scope.Subscription.subscriptionResult = true;
         $scope.Subscription.subscriptionDone = true;
+        tumblrInitialize();
         console.log("subscriptionDone: " + $scope.Subscription.subscriptionDone + " subscriptionResult: " + $scope.Subscription.subscriptionResult)
     };
 
@@ -56,6 +58,19 @@ controllers.controller('HomeCtrl', function($scope, $route, MailChimp){
 
     };
 
+    $scope.facebookShare = function(){
+      console.log("FAcebookshare");
+      window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent('http://awesome.do//#register/' + $scope.User.group), 'facebook-share-dialog', 'width=626,height=436'); 
+    return false;
+    };
+
+    function tumblrInitialize(){
+      var tumblr_link_url = "http://awesome.do/#/register/" + User.group;
+      var tumblr_link_name = "I just registered to Awesome. Join me!";
+      var tumblr_link_description = "The more we are the sooner we will get access";
+  
+      $scope.tumblrURL = "http://www.tumblr.com/share/link?url=" + encodeURIComponent(tumblr_link_url) + "&name=" + encodeURIComponent(tumblr_link_name) + "&description=" + encodeURIComponent(tumblr_link_description);
+    }
 
     function loadCarrousel(){
       var carousel = $('.carousel');
@@ -68,6 +83,8 @@ controllers.controller('HomeCtrl', function($scope, $route, MailChimp){
     }
     
     loadCarrousel();
+
+
   	
 });
 
